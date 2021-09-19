b46dd86aa541fd270946afb5832b44e4b2d272c2
mastrmind-dev <asapthaka@gmail.com>
2021-09-19 14:32:10 +0530

    99% completed from the second game/challenge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.querySelector("button").addEventListener("click", () => {
     var birthYear = prompt("What year were you born... Good Friend?");
 
-    while(isNaN(birthYear)){
+    while (isNaN(birthYear)) {
         var birthYear = prompt("What year were you born... Good Friend?", "Must enter an year");
     };
 
@@ -19,3 +19,19 @@ document.querySelectorAll('button')[1].addEventListener('click', () => {
     document.getElementById('ageInDays').remove();
 });
 
+document.querySelector("#generate-cat-button").addEventListener('click', () => {
+
+    document.querySelector("#generate-cat-button").innerText = "Generating..."
+
+    fetch("https://api.thecatapi.com/v1/images/search").then(response => (response.json())).then(data => {
+        document.getElementById("cat-image").setAttribute("src", data[0].url);
+        document.querySelector("#cat-image").style.visibility = "visible";
+        document.querySelector("#cat-image").addEventListener("load", (e) => {
+            if (e.returnValue === true) {
+                document.querySelector("#generate-cat-button").innerText = "Generate Another Cat";
+            }
+        });
+
+    });
+
+})
