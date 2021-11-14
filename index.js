a0df19f17f97d7dfe96cbbd4dbc9a0838c0432b7
Sapthaka Morahela <57357216+mastrmind-dev@users.noreply.github.com>
2021-11-14 12:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -351,7 +351,7 @@ function showCard(player) {
         cardImage.style.weight = "100px";
         cardImage.style.padding = "0.5em";
         let card = randomCard();
-        cardImage.src = `https://github.com/mastrmind-dev/Javascript-Small-Games-Collection/blob/main/assets/blackjack_Images/${card}.png`;
+        cardImage.src = `https://raw.githubusercontent.com/mastrmind-dev/Javascript-Small-Games-Collection/main/assets/blackjack_Images/${card}.png`;
         document.querySelector(player.div).appendChild(cardImage);
         showScore(card, player, cardNo);
 
